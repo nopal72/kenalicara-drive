d5f985a16da732a6a36f876f7fdea7759e743db0
nopal72 <117352944+nopal72@users.noreply.github.com>
2026-04-13 15:22:29 +0700

    feat: change model and optimized get models

--- gas/model.js
+++ gas/model.js
@@ -1,20 +1,47 @@
-const MODEL_ID_A = "1v9FldZpsSqaWICoePCGg0zkCH69Umgmi";
-const MODEL_ID_K = "1IgWTRL16YtehF25yAyTUgg6ggWAPdAVI";
-const MODEL_ID_V = "1_aCPKS7QCRnaOZ4udw9ex5yp2hPq1AbP";
+const MODEL_ID_A = "1kUyi6TYJ1-8RPT6b6nKmrATCtRujV37x";
+const MODEL_ID_K = "1VY7CqsPe8ixMjD-zyhh8fXOwFicY9EjX";
+const MODEL_ID_V = "1m9JUBzKeuZX1QZKGayr5SmNJE2fYTqkl";
 
-var CACHED_MODELS = null;
+var CACHED_MODELS;
+
+function getModels() {
+  if (CACHED_MODELS) {
+    return CACHED_MODELS;
+  }
+
+  console.time("LOAD_MODEL");
+
+  CACHED_MODELS = {
+    A: JSON.parse(DriveApp.getFileById(MODEL_ID_A).getBlob().getDataAsString()),
+    K: JSON.parse(DriveApp.getFileById(MODEL_ID_K).getBlob().getDataAsString()),
+    V: JSON.parse(DriveApp.getFileById(MODEL_ID_V).getBlob().getDataAsString())
+  };
+
+  console.timeEnd("LOAD_MODEL");
+
+  return CACHED_MODELS;
+}
+
 
 function testPredict() {
   // data dummy
   const dummyData = {
-    a1: 5, a2: 5, a3: 5, a4: 5, a5: 5,
-    v1: 1, v2: 1, v3: 1, v4: 1, v5: 1,
-    k1: 5, k2: 5, k3: 5, k4: 5, k5: 5
+    a1: 4, a2: 4, a3: 4, a4: 4, a5: 4,
+    v1: 4, v2: 4, v3: 4, v4: 4, v5: 4,
+    k1: 1, k2: 1, k3: 1, k4: 1, k5: 1
   }
 
   const fullData = addConstructedFeatures(dummyData);
 
   return predictXgboost(fullData)
+}
+
+function testLoop() {
+  for (let i = 0; i < 3; i++) {
+    console.time("RUN " + i);
+    testPredict();
+    console.timeEnd("RUN " + i);
+  }
 }
 
 function addConstructedFeatures(data) {
@@ -67,19 +94,6 @@ function predictXgboost(data) {
   };
 }
 
-// function load model from google drive
-function getModels() {
-  if (CACHED_MODELS) return CACHED_MODELS;
-
-  console.log("Memuat 3 model dari Drive...");
-  const modelA = JSON.parse(DriveApp.getFileById(MODEL_ID_A).getBlob().getDataAsString());
-  const modelK = JSON.parse(DriveApp.getFileById(MODEL_ID_K).getBlob().getDataAsString());
-  const modelV = JSON.parse(DriveApp.getFileById(MODEL_ID_V).getBlob().getDataAsString());
-
-  CACHED_MODELS = { A: modelA, K: modelK, V: modelV };
-  return CACHED_MODELS;
-}
-
 // function predict 
 function predictSingleModel(modelJson, inputs) {
   const learner = modelJson.learner;
@@ -119,4 +133,17 @@ function predictSingleModel(modelJson, inputs) {
   }
 
   return 1 / (1 + Math.exp(-rawScore));
+
+  // function load model from google drive
+  // function getModels() {
+  //   if (CACHED_MODELS) return CACHED_MODELS;
+
+  //   console.log("Memuat 3 model dari Drive...");
+  //   const modelA = JSON.parse(DriveApp.getFileById(MODEL_ID_A).getBlob().getDataAsString());
+  //   const modelK = JSON.parse(DriveApp.getFileById(MODEL_ID_K).getBlob().getDataAsString());
+  //   const modelV = JSON.parse(DriveApp.getFileById(MODEL_ID_V).getBlob().getDataAsString());
+
+  //   CACHED_MODELS = { A: modelA, K: modelK, V: modelV };
+  //   return CACHED_MODELS;
+  // }
 }
